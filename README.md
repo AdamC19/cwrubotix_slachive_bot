db7f631f33174adf427ec96634c2bc93ea999bd9
AdamC19 <cordingleyadam19+github@gmail.com>
2018-04-07 12:45:48 -0400

    Flesh out README, bot.py spits out JSON

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # cwrubotix_slachive_bot
-A hack for stickin' it to the man.
+A workaround for slack's free account storage limit
 ======
-### Description
+## Description
 This is a bot that will save messages from the CWRUbotix slack channel and make them available for later, so that we can archive more than 10,000 messages because the premium slack is hella expensive.
 
-### About the Name
+## Setting up the Slack App
+### Relevant Documentation
+* [Slack API](https://api.slack.com/)
+* [Python SlackClient](https://pypi.python.org/pypi/slackclient)
+
+### Required Scopes
+* ```channels:history```
+   Access user’s public channels
+* ```channels:read```
+   Access information about user’s public channels
+* ```chat:write:bot```
+   Send messages as archive
+* ```team:read```
+   Access information about your workspace
+
+
+## About the Name
 "slachive" is a portmanteau of "slack" and "archive."
--- bot.py
+++ bot.py
@@ -6,17 +6,39 @@ import time
 import os.path
 import codecs
 import secrets
+import json
 
-
+#===============================================================================
+def sep(n):
+	for i in range(0,n):
+		print('*'*80)
+#===============================================================================
 def main():
 	token = open("slack_token.dat","r").read()
 
 	sc = SlackClient(token)
 
-	response = sc.api_call("api.test")
-print(response)
+	#response  		= sc.api_call("api.test")
+	#auth_resp 		= sc.api_call("auth.test")
+	channels_list 	= sc.api_call("channels.list")
+	history   		= sc.api_call("channels.history")
 
-print('*'*80)
+	#sep(2)
+	#print(response)
+	#sep(2)
+	#print(auth_resp)
+	#sep(2)
+	#channelsJSON = channels_list.encode('utf_8', 'strict')
+	#print(channels_list)
+	sep(2)
+#	print(history)
+#	sep(2)
 
-auth_resp = sc.api_call("auth.test")
-print(auth_resp)
+	#json.loads(channels_list)
+	print(json.dumps(channels_list) )
+	#for key in channels_list:
+	#	print(channels_list[key])
+
+#===============================================================================
+if __name__ == '__main__':
+	main()
